72c21e1989866ad7a577e392107d4134b31339eb
faizalhafiz66 <faizalhafiz66@gmail.com>
2018-08-20 15:53:03 +0700

    change

--- src/main/java/com/Javabootcamp/spring/TestcaseApplication.java
+++ src/main/java/com/Javabootcamp/spring/TestcaseApplication.java
@@ -1,4 +1,4 @@
-package com.Javabootcamp.spring.testcase;
+package com.Javabootcamp.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
